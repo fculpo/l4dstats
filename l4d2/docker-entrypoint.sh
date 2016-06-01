cb793a4789489e7d3e33b90d3fe9997954babc4b
Binal <binal@bath.com>
2016-06-01 18:49:27 +0200

    working

--- l4d2/docker-entrypoint.sh
+++ l4d2/docker-entrypoint.sh
@@ -2,8 +2,17 @@
 set -e
 
 if [ "$1" = 'l4d2' ]; then
-  chown -R steam /opt/steam/
+  # Create appid if necessary
+  if [ ! -e /opt/steam/l4d2/steam_appid.txt ]; then
+    echo 550 >  /opt/steam/l4d2/steam_appid.txt
+  fi
+  
+  # Take ownership on possible mounted volumes
+  chown -R steam:steam /opt/steam/l4d2/left4dead2/addons
+  
+  # Launch server as steam user
   exec gosu steam /opt/steam/l4d2/srcds_run -game left4dead2 -usercon -ip 0.0.0.0
 fi
 
+# Launch command if provided (as root)
 exec "$@"
